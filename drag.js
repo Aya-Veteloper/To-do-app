9f427962af2124a609d5803b72fb69cfbc471585
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2023-08-06 20:22:04 +0200

    upload

---
+++ drag.js
@@ -0,0 +1,81 @@
+const Tasks = document.querySelectorAll('.task');
+const Lanes = document.querySelectorAll('.swim-lane');
+let draggedItem = null;
+
+Tasks.forEach(task => {
+  task.addEventListener('dragstart', (e) => {
+    draggedItem = task;
+    setTimeout(() => {
+      task.style.display = 'none';
+    }, 0);
+  });
+
+  task.addEventListener('dragend', () => {
+    setTimeout(() => {
+      draggedItem.style.display = 'block';
+      draggedItem = null;
+    }, 0);
+  });
+});
+
+
+Lanes.forEach(lane => {
+  lane.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  lane.addEventListener('drop', (e) => {
+    e.preventDefault();
+    lane.appendChild(draggedItem);
+  });
+});
+
+
+
+
+// const draggables = document.querySelectorAll(".task");
+// const droppables = document.querySelectorAll(".swim-lane");
+
+// draggables.forEach((task) => {
+//   task.addEventListener("dragstart", () => {
+//     task.classList.add("is-dragging");
+//   });
+//   task.addEventListener("dragend", () => {
+//     task.classList.remove("is-dragging");
+//   });
+// });
+
+// droppables.forEach((zone) => {
+//   zone.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+
+//     const bottomTask = insertAboveTask(zone, e.clientY);
+//     const curTask = document.querySelector(".is-dragging");
+
+//     if (!bottomTask) {
+//       zone.appendChild(curTask);
+//     } else {
+//       zone.insertBefore(curTask, bottomTask);
+//     }
+//   });
+// });
+
+// const insertAboveTask = (zone, mouseY) => {
+//   const els = zone.querySelectorAll(".task:not(.is-dragging)");
+
+//   let closestTask = null;
+//   let closestOffset = Number.NEGATIVE_INFINITY;
+
+//   els.forEach((task) => {
+//     const { top } = task.getBoundingClientRect();
+
+//     const offset = mouseY - top;
+
+//     if (offset < 0 && offset > closestOffset) {
+//       closestOffset = offset;
+//       closestTask = task;
+//     }
+//   });
+
+//   return closestTask;
+// };
